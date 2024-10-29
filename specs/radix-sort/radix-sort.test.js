d1923f5f57e1df77647e2de0f71403ca5e0b9f06
Gujjala Dheeraj Chandra <dheerug773@gmail.com>
2024-10-29 21:56:57 +0530

    radix sort

--- specs/radix-sort/radix-sort.test.js
+++ specs/radix-sort/radix-sort.test.js
@@ -9,60 +9,54 @@
 
 */
 
+const getDigit = (number, place) => {
+  while (place > 0) {
+    number = Math.floor(number / 10);
+    place--;
+  }
+
+  return number % 10;
+};
+
+const getLong = (array) => {
+  let resl = 0;
+  for (let item of array) {
+    const curr = item.toString().length;
+    resl = curr > resl ? curr : resl;
+  }
+  return resl;
+};
+
 function radixSort(array) {
   // code goes here
+  const max = getLong(array);
+  let m = new Array(10).fill().map(() => []);
+  for (let i = 0; i < max; i++) {
+    while (array.length) {
+      const latest = array.shift();
+      m[getDigit(latest, i)].push(latest);
+    }
+    for (let k = 0; k < 10; k++) {
+      while (m[k].length) {
+        array.push(m[k].shift());
+      }
+    }
+  }
+  return array;
 }
 
 // unit tests
 // do not modify the below code
-describe.skip("radix sort", function () {
+describe("radix sort", function () {
   it("should sort correctly", () => {
     const nums = [
-      20,
-      51,
-      3,
-      801,
-      415,
-      62,
-      4,
-      17,
-      19,
-      11,
-      1,
-      100,
-      1244,
-      104,
-      944,
-      854,
-      34,
-      3000,
-      3001,
-      1200,
-      633
+      20, 51, 3, 801, 415, 62, 4, 17, 19, 11, 1, 100, 1244, 104, 944, 854, 34,
+      3000, 3001, 1200, 633,
     ];
     const ans = radixSort(nums);
     expect(ans).toEqual([
-      1,
-      3,
-      4,
-      11,
-      17,
-      19,
-      20,
-      34,
-      51,
-      62,
-      100,
-      104,
-      415,
-      633,
-      801,
-      854,
-      944,
-      1200,
-      1244,
-      3000,
-      3001
+      1, 3, 4, 11, 17, 19, 20, 34, 51, 62, 100, 104, 415, 633, 801, 854, 944,
+      1200, 1244, 3000, 3001,
     ]);
   });
   it("should sort 99 random numbers correctly", () => {
