7fa24b079df4f1bd37603fbd8953df8e26dabf73
Gujjala Dheeraj Chandra <dheerug773@gmail.com>
2024-10-29 15:13:13 +0530

    quick sort

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -14,11 +14,28 @@
 
 function quickSort(nums) {
   // code goes here
+  if (nums.length < 2) {
+    return nums;
+  }
+  let left = [];
+  let right = [];
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < nums[nums.length - 1]) {
+      left.push(nums[i]);
+    } else {
+      right.push(nums[i]);
+    }
+  }
+  return Array.prototype.concat(
+    quickSort(left),
+    [nums[nums.length - 1]],
+    quickSort(right)
+  );
 }
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+test("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
